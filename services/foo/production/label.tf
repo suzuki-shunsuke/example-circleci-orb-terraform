7bbf026dc30e51522b8a829f8f92b29dd8f31d25
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-03-10 22:04:07 +0900

    no states are changed

--- services/foo/production/label.tf
+++ services/foo/production/label.tf
@@ -1,5 +1,5 @@
-resource "github_issue_label" "test_repo" {
-  repository = "example-circleci-orb-tfenv"
-  name       = "foo-production"
-  color      = "FF0000"
-}
+# resource "github_issue_label" "test_repo" {
+#   repository = "example-circleci-orb-tfenv"
+#   name       = "foo-production"
+#   color      = "FF0000"
+# }
