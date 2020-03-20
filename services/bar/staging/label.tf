e0e8cce1913a6805467723eb80ba08d6e7f1828c
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2020-03-20 15:17:00 +0900

    Test

--- services/bar/staging/label.tf
+++ services/bar/staging/label.tf
@@ -1,5 +1,5 @@
 resource "github_issue_label" "test_repo" {
   repository = "example-circleci-orb-terraform"
   name       = "bar-staging"
-  color      = "FF0000"
+  color        = "FF0000"
 }
--- services/foo/production/label.tf
+++ services/foo/production/label.tf
@@ -2,4 +2,5 @@ resource "github_issue_label" "test_repo" {
   repository = "example-circleci-orb-terraform"
   name       = "foo-production"
   color      = "FF0000"
+  foo        = "bar"
 }
